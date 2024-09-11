c65cf3e4f5cdf578ff76ca2a7cdbba2eb9bbe26c
cherkaoui oussama <oussamacherkaoui2019@gmail.com>
2024-09-11 13:48:37 +0100

    ajouter du configuration openfeign

--- Task/src/main/java/org/task/Project/ProjectRest.java
+++ Task/src/main/java/org/task/Project/ProjectRest.java
@@ -1,11 +1,13 @@
 package org.task.Project;
 
+import feign.Headers;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "PROJECT")
 public interface ProjectRest {
+
     @GetMapping("/apiProject/isExist/{id}")
     Boolean isExist(@PathVariable Long id);
 }
--- Task/src/main/java/org/task/TaskApplication.java
+++ Task/src/main/java/org/task/TaskApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 
+@EnableFeignClients
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
 public class TaskApplication {
 
     public static void main(String[] args) {
